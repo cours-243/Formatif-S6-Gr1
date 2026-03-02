21c8a237bf22090a554ea0ee04f1dfa7a063a807
HadjerBenha <2546970@cegeplimoilou.ca>
2026-03-02 13:17:37 -0500

    modification implementation de la methode manger du PoissonClown

--- src/HadjerBenhagouga/aquarium/PoissonClown.java
+++ src/HadjerBenhagouga/aquarium/PoissonClown.java
@@ -1,13 +1,17 @@
-package etudiant10.aquarium;
+package HadjerBenhagouga.aquarium;
 
 public class PoissonClown extends Poisson {
 
     public PoissonClown(String nom, int age, String couleur) {
+
         super(nom, age, couleur);
+
+
     }
 
     @Override
     public void manger() {
-        System.out.println(nom + " mange des frites au ketchup");
+
+        System.out.println(this.nom + " mange des frites au ketchup");
     }
 }
