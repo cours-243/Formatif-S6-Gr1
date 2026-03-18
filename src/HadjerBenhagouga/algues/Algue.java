a1f89d04ce9a98383cf73b1d1b23012ac2ecf966
HadjerBenha <2546970@cegeplimoilou.ca>
2026-03-18 14:54:05 -0400

    Create Algue.java
     Ajout du package algues +
    Ajout de la classe  Algue et implémentation  de la  méthode toString()

---
+++ src/HadjerBenhagouga/algues/Algue.java
@@ -0,0 +1,15 @@
+package HadjerBenhagouga.aquarium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algue {
+
+    public Algue() {
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Algue{}";
+    }
+}
